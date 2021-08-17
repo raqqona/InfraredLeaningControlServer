9aacbe5de2017679498ad7c63f850d97c93780a1
raqqona <e1n18047@st.oit.ac.jp>
2021-08-17 17:51:15 +0900

    change model for json

--- model/aircontroller.go
+++ model/aircontroller.go
@@ -1,16 +1,16 @@
 package model
 
 type AirController struct {
-    Power byte
-    Mode byte
-    Temp byte
-    Swing byte
-    Fan byte
-    LastCommand bool
+    Power byte 'json:"field_byte"'
+    Mode byte 'json:"field_byte"'
+    Temp byte 'json:"field_byte"'
+    Swing byte 'json:"field_byte"'
+    Fan byte 'json:"field_byte"'
+    LastCommand bool 'json:"field_bool"'
 }
 
 type IndoorEnvironment struct {
-    Temp float64
-    Hum float64
-    Press float64
+    Temp float64 'json:"field_float64"'
+    Hum float64 'json:"field_float64"'
+    Press float64 'json:"field_float64"'
 }
