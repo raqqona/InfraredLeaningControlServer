469e3a1a112a4ed267ca3247b3f0cc9285da494f
raqqona <e1n18047@st.oit.ac.jp>
2021-08-18 22:29:51 +0900

    add get User-Agent in middleware

--- middleware/aircontrollerMiddleware.go
+++ middleware/aircontrollerMiddleware.go
@@ -13,9 +13,11 @@ func RecordTime(c *gin.Context) {
         log.Fatal(err.Error())
     }
     oldTime := time.Now()
+    ua := c.GetHeader('User-Agent")
     c.Next()
      logger.Info("incoming request",
         zap.String("path", c.Request.URL.Path),
+        zap.String('User-Agent', ua),
         zap.Int("status", c.Writer.Status()),
         zap.Duration("elapsed", time.Now().Sub(oldTime)),
     )
