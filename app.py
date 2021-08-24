bfce560a98cbeb3c8c57d5d6eb0027bea88bc4f3
raqqona <e1n18047@st.oit.ac.jp>
2021-08-24 22:29:27 +0900

    change python from go

---
+++ app.py
@@ -0,0 +1,21 @@
+from Flask import Flask, request, Response, make_response
+import sqlite3
+import os
+
+app = Flask(__name__)
+
+@app.errorhandler(500)
+def error_500(error):
+    return 500
+
+@app.route('/api/pohling', method=['GET'])
+def pohling():
+    pass
+
+@app.route('/api/getIndoorEnv', method=['GET'])
+def pohling():
+    pass
+
+@app.route('/api/command', method=['POST'])
+def pohling():
+    pass
