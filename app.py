388ed5c938a12b2ea256b0c73e698d5e1af3f5e5
raqqona <e1n18047@st.oit.ac.jp>
2021-08-25 23:59:18 +0900

    add setter each class member

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, Response, make_response
 import sqlite3
 import os
-import model/airconcommand
-import model/indoorenvironment
+from .model import airconcommand
+from .model import indoorenvironment
 
 app = Flask(__name__)
 
@@ -12,12 +12,27 @@ def error_500(error):
 
 @app.route('/api/pohling', method=['GET'])
 def pohling():
+    indoor_env = indoorenvironment.IndoorEnv()
+    indoor_env.set_data_from_json(request.json)
+
+    command = airconcommand.AirConCommnad()
+
+    command_json = get_command()
+    
     return command_json
 
 @app.route('/api/getIndoorEnv', method=['GET'])
 def getIndoorEnv():
+    indoor_env = indoorenvironment.IndoorEnv()
+    indoor_env.set_data_from_sql(get_indoorenv())
+
     return indoorEnv_json
 
 @app.route('/api/command', method=['POST'])
 def command():
+    command = airconcommand.AirConCommnad()
+    command.set_data_from_json(request.json)
+
+    insert_command(command)
+    
     return "accept"
