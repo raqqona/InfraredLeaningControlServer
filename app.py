3d584bd56dd16baed298a1206cbecae8ddb8b0b1
raqqona <e1n18047@st.oit.ac.jp>
2021-08-28 22:54:41 +0900

    comp prototype

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, Response, make_response
 import sqlite3
 import os
-from .model import indoorenvironment
-from .model import airconcommand
-from .service import sql
+import model
+import sql
+
+
 
 INDOORENV_ID = 0
 COMMAND_ID = 0
 
+sql_hadller = sql.SqlHandler()
 
 app = Flask(__name__)
 
@@ -16,33 +18,34 @@ def error_500(error):
     return 500
 
 @app.route('/api/pohling', methods=['GET'])
-def pohling(sql_hadller):
-    indoor_env = indoorenvironment.IndoorEnv()
+def pohling():
+    indoor_env = model.IndoorEnv()
     indoor_env.set_data_from_json(request.json)
     INDOORENV_ID =sql_hadller.insert_indoorEnv_column(indoor_env)
 
-    command = airconcommand.AirConCommnad()
-
-    
-    return command.export_json()
+    command = model.AirConCommnad()
+    if !COMMAND_ID < sql_handler.get_latest_command():
+        return command.export_json(False)
+    else:
+        return command.export_json(True)
+        
 
 @app.route('/api/getIndoorEnv', methods=['GET'])
-def getIndoorEnv(sql_hadller):
-    indoor_env = indoorenvironment.IndoorEnv()
+def getIndoorEnv():
+    indoor_env = model.IndoorEnv()
     indoor_env.set_data_from_sql(sql_hadller.get_latest_indoorEnv())
 
-    return indoor_env.export_json
+    return indoor_env.export_json()
 
 @app.route('/api/command', methods=['POST'])
-def command(sql_hadller):
-    command = airconcommand.AirConCommnad()
+def command():
+    command = model.AirConCommnad()
     command.set_data_from_json(request.json)
-    COMMAND_ID = sq_hadllerl.insert_command_column(command)
+    COMMAND_ID = sql_hadllerl.insert_command_column(command)
 
-    sql.insert_command(command)
+    sql_handler.insert_command(command)
     
     return "accept"
 
 if __name__ == '__main__':
-    sql_hadller = sql.SqlHandler()
-    main(sql_hadller)
+    app.run(debug=False, host='0.0.0.0', port=8080)
---
+++ sql.py
@@ -0,0 +1,58 @@
+import sqlite3
+import os
+import model
+
+
+INDOORENV_TABLE = "indoorEnvironment_table"
+COMMAND_TABLE = "command_table"
+DB_PATH = "**********"
+
+class SqlHandler:
+    def __init__(self):
+        self.conn = sqlite3.connect(DB_PATH)
+        self.cur = self.conn.cursor()
+
+    def insert_indoorEnv_column(self, column):
+        cur.execute('INSERT INTO ? VALUES (?, ?, ?)', [INDOORENV_TABLE, column.temp, temp.hum, temp.press])
+        conn.commit()
+        
+        return get_latest_id(table_name)
+    
+    def insert_command_column(self, column):
+        cur.execute('INSERT INTO ? VALUES (?, ?, ?, ?, ?)', [COMMAND_TABLE, column.power, column.mode, column.temp, column.swing, column.fan])
+        conn.commit()
+        
+        return get_latest_id(table_name)
+    
+    def delete_column(self, table_name, id):
+        cur.execute('DELETE FROM ? WHERE id = ?', [table_name, id])
+        conn.commit()
+
+        return get_latest_id(table_name)
+
+    def get_column(self, table_name, id):
+        cur.execute('SELECT * FROM ' + table_name)
+        column = cur.fetchall()
+
+        return column
+    
+    def get_latest_id(self, table_name):
+        cur.execute('SELECT id FORM ' + table_name)
+        id_row = cur.fetchall() 
+        return max(id_row)
+    
+    def get_latest_indoorEnv(self):
+        id = get_latest_id(self)
+        indoorEnv_column = get_column(INDOORENV_TABLE, id)
+        indoorEnv = indoorenvironment.IndoorEnv(indoorEnv_column)
+        
+        return indoorEnv
+    
+    
+    def get_latest_commnad(self):
+        id = get_latest_id(self)
+        command_column = get_column(COMMAND_TABLE, id)
+        command = airconcommand.AirConCommnad(command_column)
+
+        return command
+    
