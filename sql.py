292d9beefb52ad205f108a02c7e37c18565c331c
raqqona <e1n18047@st.oit.ac.jp>
2021-12-31 20:25:21 +0900

    insert try-exception

--- sql.py
+++ sql.py
@@ -13,7 +13,8 @@ class SqlHandler:
         self.cur = self.conn.cursor()
 
     def insert_indoorEnv_column(self, column):
-        cur.execute('INSERT INTO ' + INDOORENV_TABLE +' VALUES (?, ?, ?)', (column.temp, temp.hum, temp.press))
+        try:
+            self.cur.execute('INSERT INTO ' + INDOORENV_TABLE +' VALUES (?, ?, ?)', (column.temp, temp.hum, temp.press))
         
         except sqlite3.Error as e:
             print(e)
@@ -21,7 +22,8 @@ class SqlHandler:
         conn.commit()
     
     def insert_command_column(self, column):
-        cur.execute('INSERT INTO ' + COMMAND_TABLE + ' VALUES (?, ?, ?, ?, ?)', (column.power, column.mode, column.temp, column.swing, column.fan))
+        try:
+            self.cur.execute('INSERT INTO ' + COMMAND_TABLE + ' VALUES (?, ?, ?, ?, ?)', (column.power, column.mode, column.temp, column.swing, column.fan))
         
         except sqlite3.Error as e:
             print(e)
@@ -29,7 +31,8 @@ class SqlHandler:
         conn.commit()
     
     def delete_column(self, table_name, id):
-        cur.execute('DELETE FROM ' + table_name + ' WHERE id = ?', (id))
+        try:
+            self.cur.execute('DELETE FROM ' + table_name + ' WHERE id = ?', (id))
 
         except sqlite3.Error as e:
             print(e)
@@ -37,37 +40,45 @@ class SqlHandler:
         conn.commit()
 
     def get_column(self, table_name, id):
-        cur.execute('SELECT * FROM ' + table_name + ' WHERE id=?', (id))
+        try:
+            self.cur.execute('SELECT * FROM ' + table_name + ' WHERE id=?', (id))
 
         except sqlite3.Error as e:
             print(e)
 
-        column = cur.fetchall()
+        column = self.cur.fetchall()
         
         return column
     
     def get_latest_id(self, table_name):
-        cur.execute('SELECT id FORM ' + table_name)
+        try:
+            self.cur.execute('SELECT id FORM ' + table_name)
         
         except sqlite3.Error as e:
             print(e)
 
-        id_row = cur.fetchall() 
+        id_row = self.cur.fetchall() 
    
         return max(id_row)
     
     def get_latest_indoorEnv(self):
-        id = get_latest_id(get_latest_id(INDOORENV_TABLE))
-        indoorEnv_column = get_column(INDOORENV_TABLE, id)
-        indoorEnv = model.IndoorEnv(indoorEnv_column)
+        try:
+            id = get_latest_id(get_latest_id(INDOORENV_TABLE))
+            indoorEnv_column = get_column(INDOORENV_TABLE, id)
+            indoorEnv = model.IndoorEnv(indoorEnv_column)
+        except sqlite3.Error as e:
+            print(e)
         
         return indoorEnv
     
     
     def get_latest_commnad(self):
-        id = get_latest_id(get_latest_id(COMMAND_TABLE))
-        command_column = get_column(COMMAND_TABLE, id)
-        command = model.AirConCommnad(command_column)
+        try:
+            id = get_latest_id(get_latest_id(COMMAND_TABLE))
+            command_column = get_column(COMMAND_TABLE, id)
+            command = model.AirConCommnad(command_column)
+        except sqlite3.Error as e:
+            print(e)
 
         return command
     
